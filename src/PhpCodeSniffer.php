dbf1b0f946258990654e8f96d38ceb30e3741188
Dmitry Khomutov <poisoncorpsee@gmail.com>
2019-12-12 00:26:21 +0700

    Added plugins: PhpCodeSniffer, PhpMessDetector, SecurityChecker.

---
+++ src/PhpCodeSniffer.php
@@ -0,0 +1,262 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PHPCensor\Plugins;
+
+use PHPCensor\Common\Build\BuildErrorInterface;
+use PHPCensor\Common\Build\BuildInterface;
+use PHPCensor\Common\Exception\Exception;
+use PHPCensor\Common\Plugin\Plugin;
+use PHPCensor\Common\Plugin\ZeroConfigPluginInterface;
+
+/**
+ * PHP Code Sniffer Plugin - Allows PHP Code Sniffer testing.
+ *
+ * @package    PHP Censor
+ * @subpackage Plugins
+ *
+ * @author Dmitry Khomutov <poisoncorpsee@gmail.com>
+ * @author Dan Cryer <dan@block8.co.uk>
+ */
+class PhpCodeSniffer extends Plugin implements ZeroConfigPluginInterface
+{
+    /**
+     * @var array
+     */
+    protected $suffixes = ['php'];
+
+    /**
+     * @var string
+     */
+    protected $standard = 'PSR2';
+
+    /**
+     * @var string
+     */
+    protected $tabWidth = '';
+
+    /**
+     * @var string
+     */
+    protected $encoding = '';
+
+    /**
+     * @var int
+     */
+    protected $allowedErrors = 0;
+
+    /**
+     * @var int
+     */
+    protected $allowedWarnings = 0;
+
+    /**
+     * @var int
+     */
+    protected $severity = null;
+    /**
+     * @var null|int
+     */
+    protected $errorSeverity = null;
+
+    /**
+     * @var null|int
+     */
+    protected $warningSeverity = null;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getName(): string
+    {
+        return 'php_code_sniffer';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(): bool
+    {
+        list($ignores, $standard, $suffixes, $severity, $errorSeverity, $warningSeverity) = $this->getFlags();
+
+        $executable = $this->commandExecutor->findBinary($this->binaryNames, $this->binaryPath);
+
+        if ((!\defined('DEBUG_MODE') || !DEBUG_MODE) &&
+            !(bool)$this->build->getExtra('debug')) {
+            $this->commandExecutor->disableCommandOutput();
+        }
+
+        $cmd = 'cd "%s" && ' . $executable . ' --report=json %s %s %s %s %s "%s" %s %s %s';
+        $this->commandExecutor->executeCommand(
+            $cmd,
+            $this->build->getBuildPath(),
+            $standard,
+            $suffixes,
+            $ignores,
+            $this->tabWidth,
+            $this->encoding,
+            $this->directory,
+            $severity,
+            $errorSeverity,
+            $warningSeverity
+        );
+
+        $output                  = $this->commandExecutor->getLastCommandOutput();
+        list($errors, $warnings) = $this->processReport($output);
+
+        $this->commandExecutor->enableCommandOutput();
+
+        $success = true;
+
+        $this->buildMetaWriter->write(
+            $this->build->getId(),
+            (self::getName() . '-warnings'),
+            (string)$warnings
+        );
+
+        $this->buildMetaWriter->write(
+            $this->build->getId(),
+            (self::getName() . '-errors'),
+            (string)$errors
+        );
+
+        if (-1 !== $this->allowedWarnings && $warnings > $this->allowedWarnings) {
+            $success = false;
+        }
+
+        if (-1 !== $this->allowedErrors && $errors > $this->allowedErrors) {
+            $success = false;
+        }
+
+        return $success;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function canExecuteOnStage(string $stage, BuildInterface $build): bool
+    {
+        if (BuildInterface::STAGE_TEST === $stage) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function initPluginSettings(): void
+    {
+        if ($this->options->has('zero_config') && $this->options->get('zero_config', false)) {
+            $this->allowedWarnings = -1;
+            $this->allowedErrors   = -1;
+        }
+
+        $this->allowedErrors   = (int)$this->options->get('allowed_errors', $this->allowedErrors);
+        $this->allowedWarnings = (int)$this->options->get('allowed_warnings', $this->allowedWarnings);
+        $this->suffixes        = (array)$this->options->get('suffixes', $this->suffixes);
+        $this->standard        = $this->options->get('standard', $this->standard);
+        $this->severity        = $this->options->get('severity', $this->severity);
+        $this->errorSeverity   = $this->options->get('error_severity', $this->errorSeverity);
+        $this->warningSeverity = $this->options->get('warning_severity', $this->warningSeverity);
+
+        if ($tabWidth = $this->options->get('tab_width')) {
+            $this->tabWidth = ' --tab-width=' . $tabWidth;
+        }
+
+        if ($encoding = $this->options->get('encoding')) {
+            $this->tabWidth = ' --encoding=' . $encoding;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getPluginDefaultBinaryNames(): array
+    {
+        return [
+            'phpcs',
+            'phpcs.phar',
+        ];
+    }
+
+    protected function getFlags(): array
+    {
+        $ignoreString = '';
+        if ($this->ignores) {
+            $ignoreString = \sprintf(' --ignore="%s"', \implode(',', $this->ignores));
+        }
+
+        $standardPath = $this->pathResolver->resolvePath($this->standard, true);
+        if (\file_exists($standardPath)) {
+            $standard = ' --standard=' . $standardPath;
+        } else {
+            $standard = ' --standard=' . $this->standard;
+        }
+
+        $suffixes = '';
+        if ($this->suffixes) {
+            $suffixes = ' --extensions=' . \implode(',', $this->suffixes);
+        }
+
+        $severity = '';
+        if (null !== $this->severity) {
+            $severity = ' --severity=' . $this->severity;
+        }
+
+        $errorSeverity = '';
+        if (null !== $this->errorSeverity) {
+            $errorSeverity = ' --error-severity=' . $this->errorSeverity;
+        }
+
+        $warningSeverity = '';
+        if (null !== $this->warningSeverity) {
+            $warningSeverity = ' --warning-severity=' . $this->warningSeverity;
+        }
+
+        return [$ignoreString, $standard, $suffixes, $severity, $errorSeverity, $warningSeverity];
+    }
+
+    /**
+     * @param string $output
+     *
+     * @return array
+     *
+     * @throws Exception
+     */
+    protected function processReport(string $output): array
+    {
+        $data = \json_decode(\trim($output), true);
+
+        if (!\is_array($data)) {
+            throw new Exception(
+                'Could not process the report generated by PHP Code Sniffer. Report content: ' . $output
+            );
+        }
+
+        $errors   = $data['totals']['errors'];
+        $warnings = $data['totals']['warnings'];
+
+        foreach ($data['files'] as $fileName => $file) {
+            $fileName = \str_replace($this->build->getBuildPath(), '', $fileName);
+
+            foreach ($file['messages'] as $message) {
+                $this->buildErrorWriter->write(
+                    $this->build->getId(),
+                    self::getName(),
+                    ('PHPCS: ' . $message['message']),
+                    ('ERROR' === $message['type']
+                        ? BuildErrorInterface::SEVERITY_HIGH
+                        : BuildErrorInterface::SEVERITY_LOW
+                    ),
+                    $fileName,
+                    $message['line']
+                );
+            }
+        }
+
+        return [$errors, $warnings];
+    }
+}
---
+++ src/SecurityChecker.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PHPCensor\Plugins;
+
+use PHPCensor\Common\Build\BuildErrorInterface;
+use PHPCensor\Common\Build\BuildInterface;
+use PHPCensor\Common\Plugin\Plugin;
+use PHPCensor\Common\Plugin\ZeroConfigPluginInterface;
+use SensioLabs\Security\SecurityChecker as SensiolabsSecurityChecker;
+
+/**
+ * SensioLabs Security Checker Plugin
+ *
+ * @package    PHP Censor
+ * @subpackage Plugins
+ *
+ * @author Dmitry Khomutov <poisoncorpsee@gmail.com>
+ */
+class SecurityChecker extends Plugin implements ZeroConfigPluginInterface
+{
+    /**
+     * @var int
+     */
+    protected $allowedWarnings = 0;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getName(): string
+    {
+        return 'security_checker';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(): bool
+    {
+        $success  = true;
+        $checker  = new SensiolabsSecurityChecker();
+        $result   = $checker->check($this->build->getBuildPath() . 'composer.lock');
+        $warnings = \json_decode((string)$result, true);
+
+        if ($warnings) {
+            foreach ($warnings as $library => $warning) {
+                foreach ($warning['advisories'] as $data) {
+                    $this->buildErrorWriter->write(
+                        $this->build->getId(),
+                        self::getName(),
+                        ($library . ' (' . $warning['version'] . ")\n" . $data['cve'] . ': ' . $data['title'] . "\n" . $data['link']),
+                        BuildErrorInterface::SEVERITY_CRITICAL
+                    );
+                }
+            }
+
+            if ($this->allowedWarnings !== -1 && ($result->count() > $this->allowedWarnings)) {
+                $success = false;
+            }
+        }
+
+        return $success;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function canExecuteOnStage(string $stage, BuildInterface $build): bool
+    {
+        $path = $build->getBuildPath() . 'composer.lock';
+
+        if (\file_exists($path) && $stage === BuildInterface::STAGE_TEST) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function initPluginSettings(): void
+    {
+        if ($this->options->has('zero_config') && $this->options->get('zero_config', false)) {
+            $this->allowedWarnings = -1;
+        }
+
+        $this->allowedWarnings = (int)$this->options->get('allowed_warnings', $this->allowedWarnings);
+    }
+}
