87fc6f599df0fea7e37b91835aa38d7024180ef5
Dmitry Khomutov <poisoncorpsee@gmail.com>
2019-12-26 21:10:06 +0700

    Added plugins: DeployerOrg and Telegram.

---
+++ src/Deploy/DeployerOrg.php
@@ -0,0 +1,160 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PHPCensor\Plugins\Database;
+
+use PHPCensor\Common\Build\BuildInterface;
+use PHPCensor\Common\Plugin\Plugin;
+
+/**
+ * Deployer plugin for PHP Censor: http://deployer.org
+ *
+ * @package    PHP Censor
+ * @subpackage Plugins
+ *
+ * @author Dmitry Khomutov <poisoncorpsee@gmail.com>
+ * @author Alexey Boyko <ket4yiit@gmail.com>
+ */
+class DeployerOrg extends Plugin
+{
+    protected $branch;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getName(): string
+    {
+        return 'deployer_org';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(): bool
+    {
+        $executable = $this->commandExecutor->findBinary($this->binaryNames, $this->binaryPath);
+
+        if (null !== ($validationResult = $this->validateConfig())) {
+            $this->buildLogger->logNormal($validationResult['message']);
+
+            return $validationResult['successful'];
+        }
+
+        $branchConfig = $this->options[$this->branch];
+        $options      = $this->getOptions($branchConfig);
+        $deployerCmd  = "$executable $options";
+
+        return $this->commandExecutor->executeCommand($deployerCmd);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function canExecute(string $stage, BuildInterface $build): bool
+    {
+        if (BuildInterface::STAGE_DEPLOY === $stage) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function initPluginSettings(): void
+    {
+        $this->branch = $this->build->getBranch();
+    }
+
+    /**
+     * Validate config.
+     *
+     * $validationRes['message'] Message to log
+     * $validationRes['successful'] Plugin status that is connected with error
+     *
+     *  @return array validation result
+     */
+    protected function validateConfig(): ?array
+    {
+        if (!$this->options->all()) {
+            return [
+                'message'    => 'Can\'t find configuration for plugin!',
+                'successful' => false
+            ];
+        }
+
+        if (!$this->options->get($this->branch)) {
+            return [
+                'message'    => 'There is no specified config for this branch.',
+                'successful' => true
+            ];
+        }
+
+        $branchConf = $this->options->get($this->branch);
+        if (empty($branchConf['stage'])) {
+            return [
+                'message'    => 'There is no stage for this branch',
+                'successful' => false
+            ];
+        }
+
+        return null;
+    }
+
+    /**
+     * Get verbosity flag.
+     *
+     * @param string $verbosity User defined verbosity level
+     *
+     * @return string Verbosity flag
+     */
+    protected function getVerbosityOption($verbosity)
+    {
+        $logLevelList = [
+            'verbose'      => 'v',
+            'very verbose' => 'vv',
+            'debug'        => 'vvv',
+            'quiet'        => 'q',
+        ];
+
+        $verbosity = \strtolower(\trim($verbosity));
+        if ('normal' !== $verbosity) {
+            return '-' . $logLevelList[$verbosity];
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     * Make deployer options from config
+     *
+     * @param array $config Deployer configuration array
+     *
+     * @return string Deployer options
+     */
+    protected function getOptions($config)
+    {
+        $options = [];
+        if (!empty($config['task'])) {
+            $options[] = $config['task'];
+        } else {
+            $options[] = 'deploy';
+        }
+
+        if (!empty($config['stage'])) {
+            $options[] = $config['stage'];
+        }
+
+        if (!empty($config['verbosity'])) {
+            $options[] = $this->getVerbosityOption($config['verbosity']);
+        }
+
+        if (!empty($config['file'])) {
+            $options[] = '--file=' . $config['file'];
+        }
+
+        return implode(' ', $options);
+    }
+}
---
+++ src/Notification/Telegram.php
@@ -0,0 +1,156 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PHPCensor\Plugins\Database;
+
+use GuzzleHttp\Client as HttpClient;
+use PHPCensor\Common\Build\BuildInterface;
+use PHPCensor\Common\Exception\Exception;
+use PHPCensor\Common\Plugin\Plugin;
+
+/**
+ * Telegram Plugin
+ *
+ * @package    PHP Censor
+ * @subpackage Plugins
+ *
+ * @author Dmitry Khomutov <poisoncorpsee@gmail.com>
+ * @author LEXASOFT <lexasoft83@gmail.com>
+ */
+class Telegram extends Plugin
+{
+    protected $apiKey;
+    protected $message;
+    protected $buildMsg;
+    protected $recipients = [];
+    protected $sendLog = false;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getName(): string
+    {
+        return 'telegram';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(): bool
+    {
+        $message = $this->buildMessage();
+        $client  = new HttpClient();
+        $url     = '/bot'. $this->apiKey . '/sendMessage';
+
+        foreach ($this->recipients as $chatId) {
+            $params = [
+                'chat_id'    => $chatId,
+                'text'       => $message,
+                'parse_mode' => 'Markdown',
+            ];
+            $client->post(('https://api.telegram.org' . $url), [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                ],
+                'json' => $params,
+            ]);
+
+            if ($this->sendLog) {
+                $params = [
+                    'chat_id'    => $chatId,
+                    'text'       => $this->buildMsg,
+                    'parse_mode' => 'Markdown',
+                ];
+                $client->post(('https://api.telegram.org' . $url), [
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                    ],
+                    'json' => $params,
+                ]);
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function canExecute(string $stage, BuildInterface $build): bool
+    {
+        if (\in_array($stage, [
+            BuildInterface::STAGE_BROKEN,
+            BuildInterface::STAGE_COMPLETE,
+            BuildInterface::STAGE_FAILURE,
+            BuildInterface::STAGE_FIXED,
+            BuildInterface::STAGE_SUCCESS,
+        ], true)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function initPluginSettings(): void
+    {
+        if (!$this->options->get('api_key')) {
+            throw new Exception("Not setting telegram api_key");
+        }
+
+        if (!$this->options->get('recipients')) {
+            throw new Exception("Not setting telegram api_key");
+        }
+
+        $this->apiKey  = $this->options->get('api_key');
+        $this->message = '[%ICON_BUILD%] [%PROJECT_TITLE%](%PROJECT_LINK%)' .
+            ' - [Build #%BUILD_ID%](%BUILD_LINK%) has finished ' .
+            'for commit [%SHORT_COMMIT_ID% (%COMMITTER_EMAIL%)](%COMMIT_LINK%) ' .
+            'on branch [%BRANCH%](%BRANCH_LINK%)';
+
+        $this->message = $this->options->get('message', $this->message);
+        $this->sendLog = (bool)$this->options->get('send_log', $this->sendLog);
+
+        $recipients = $this->options->get('recipients', []);
+        if ($recipients) {
+            if (\is_string($recipients)) {
+                $this->recipients = [$recipients];
+            } elseif (\is_array($recipients)) {
+                $this->recipients = $recipients;
+            }
+        }
+    }
+
+    /**
+     * Build message.
+     * @return string
+     */
+    private function buildMessage()
+    {
+        $this->buildMsg = '';
+        $buildIcon      = $this->build->isSuccessful() ? '✅' : '❌';
+        $buildLog       = $this->build->getLog();
+        $buildLog       = \str_replace(['[0;32m', '[0;31m', '[0m', '/[0m'], '', $buildLog);
+        $buildMessages  = \explode('RUNNING PLUGIN: ', $buildLog);
+
+        foreach ($buildMessages as $bm) {
+            $pos      = \mb_strpos($bm, "\n");
+            $firstRow = \mb_substr($bm, 0, $pos);
+
+            //skip long outputs
+            if (\in_array($firstRow, ['slack_notify', 'php_loc', 'telegram'], true)) {
+                continue;
+            }
+
+            $this->buildMsg .= '*RUNNING PLUGIN: ' . $firstRow . "*\n";
+            $this->buildMsg .= $firstRow == 'composer' ? '' : ('```' . \mb_substr($bm, $pos) . '```');
+        }
+
+        return $this->variableInterpolator->interpolate(
+            \str_replace(['%ICON_BUILD%'], [$buildIcon], $this->message)
+        );
+    }
+}
